6d2b97872d1daff3ffcc727f1e63ba63b740e907
Rodolfo Mariano de Souza <rodolfo.mariano@outlook.com>
2021-03-16 14:25:49 -0300

    Criação dos componetes SideBar e o Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+interface SelectedGenreProps {
+  selectedGenre: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  },
+  movies: [{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }]
+}
+
+export function Content({selectedGenre, movies}:SelectedGenreProps) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
